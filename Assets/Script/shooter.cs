0c37636c70a323c3f8a637ef543ffaec22c0a9ab
unknown <trandinhhuy1996tv@gmail.com>
2020-07-09 14:38:34 +0700

    initial commit

---
+++ Assets/Script/shooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shooter : MonoBehaviour
+{
+    public GameObject dart;
+    private float timestamp = 0f;
+    public float spawnrate = 1f;
+    public int capacity = 10;
+    public Text capacityUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        capacityUI.text = "CAPACITY:" + capacity.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time>= timestamp && capacity>0)
+        {
+            Instantiate(dart, transform.position, Quaternion.identity);
+            timestamp = Time.time + spawnrate;
+            capacity--;
+            capacityUI.text = "CAPACITY:" + capacity.ToString();
+        }
+    }
+}
---
+++ Assets/Script/target.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class target : MonoBehaviour
+{
+    public float speed=1f;
+    public int score=0;
+    public Transform leftPoint;
+    public Transform rightPoint;
+    public Text scoreUI;
+    bool movingRight = false;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        scoreUI.text = "SCORE:0";
+        //rb.AddForce(transform.up * 30f, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= leftPoint.position.x)
+        {
+            movingRight = true;
+        }
+        if (transform.position.x >= rightPoint.position.x)
+        {
+            movingRight = false;
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (movingRight)
+        {
+            rb.MovePosition(rb.position + new Vector2(rightPoint.position.x + speed, 0f) * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + new Vector2(leftPoint.position.x - speed, 0f) * Time.fixedDeltaTime);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "dart")
+        {
+            score++;
+            scoreUI.text = "SCORE:" + score.ToString();
+            speed += 0.25f;
+            Destroy(collision.gameObject);
+        }
+    }
+}
